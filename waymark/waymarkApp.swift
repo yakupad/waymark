c396ba8810ac5f68f6658979a257da3b55894e6c
Yakup Ad (Dortnoktasifir) <ExtYakupAd@ext.d-teknoloji.com.tr>
2026-09-02 16:31:10 +0300

    Initial Commit

---
+++ waymark/waymarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  waymarkApp.swift
+//  waymark
+//
+//  Created by Yakup Ad on 2.09.2026.
+//
+
+import SwiftUI
+
+@main
+struct waymarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ waymarkTests/waymarkTests.swift
@@ -0,0 +1,18 @@
+//
+//  waymarkTests.swift
+//  waymarkTests
+//
+//  Created by Yakup Ad on 2.09.2026.
+//
+
+import Testing
+
+struct waymarkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
